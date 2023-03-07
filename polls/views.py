602c7798be392e265abc45d4c7b1988d87969058
MakAnton338 <makanton95@gmail.com>
2023-03-07 20:47:20 +0200

    Django Flake8

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,12 @@
-from django.http import HttpResponseRedirect
 from django.http import Http404
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.utils import timezone
+from django.views import generic
+
 
 from .models import Choice, Question
-from django.urls import reverse
-from django.views import generic
-from django.utils import timezone
 
 
 class IndexView(generic.ListView):
