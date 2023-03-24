69de884f277581b40a9177493a52ea36603654fe
MakAnton338 <makanton95@gmail.com>
2023-03-24 15:04:02 +0200

    HW_9

---
+++ core/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    products = models.ManyToManyField(Product)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Provider(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.OneToOneField(City, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
