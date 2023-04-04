616f5bc83944de45fcf559d564eddde9b3c2bf9e
MakAnton338 <makanton95@gmail.com>
2023-04-04 21:25:33 +0300

    HW_11

--- triangle/forms.py
+++ triangle/forms.py
@@ -1,6 +1,15 @@
 from django import forms
+from django.forms import ModelForm, fields
+
+from .models import PersonModels
 
 
 class HypotenuseForm(forms.Form):
     a = forms.IntegerField(label='Length of leg A', min_value=1)
     b = forms.IntegerField(label='Length of leg B', min_value=1)
+
+
+class PersonForm(ModelForm):
+    class Meta:
+        model = PersonModels
+        fields = ['first_name', 'last_name', 'email']
--- triangle/urls.py
+++ triangle/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = 'triangle'
 urlpatterns = [
     path('', views.triangle, name='triangle'),
+    path('user/', views.person_list, name='person_list'),
+    path('person/', views.person_register, name='person_register'),
+    path('person/<int:pk>/', views.person_upd, name='person_upd'),
     ]
